5f46e3d96964e0e9d9f30d8299301c7f1f5af03b
John Hester <hesterusmaximus@gmail.com>
2020-08-21 15:28:44 -0500

    dice stuff

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,5 +1,20 @@
 function main() {
   // Put your code here
+  const rollSomeDice = () => {
+    console.log("Let's roll some dice, baby!")
+    console.log("---------------------------")
+
+    for (let i = 0; i < 10; i++) {
+      console.log(roll())
+    }
+
+  }
+
+  const roll = () => {
+    return Math.floor((Math.random() * 6) + 1)
+  }
+
+  rollSomeDice()
 
 }
 
