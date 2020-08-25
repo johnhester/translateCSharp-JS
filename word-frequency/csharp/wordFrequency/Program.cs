8558602546a902dd4a87313d0a8ae27fb9a4fb92
John Hester <hesterusmaximus@gmail.com>
2020-08-25 12:41:38 -0500

    word frequency initialized

---
+++ word-frequency/csharp/wordFrequency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wordFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
